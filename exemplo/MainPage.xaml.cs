88f71b72938034e547bcc6b7ff43a1a64557e935
Gabriel Nascimento <gabrielpedroliva@gmail.com>
2024-03-06 18:18:38 -0300

    feat: adionando enviar mensagem

---
+++ exemplo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace exemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        string name = "";
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnMessageClicked(object sender, EventArgs e)
+        {
+            name = NameEntry.Text;
+            MessageLabel.Text = $"Hello {name}!";
+        }
+    }
+     
+
+}
